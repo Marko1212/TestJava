cef968f5dbbbc944f24012f90869ab1118c74d78
Marko Askovic <marko.askovic@optopartner.com>
2025-08-06 17:23:18 +0200

    update

--- src/test/java/com/example/TextEditorBufferTest.java
+++ src/test/java/com/example/TextEditorBufferTest.java
@@ -40,5 +40,45 @@ public class TextEditorBufferTest {
         buffer.delete(10);
         assertThat(buffer.getText()).isEqualTo("");
     }
+
+    @Test
+    void appendNullShouldDoNothing() {
+        TextEditorBuffer buffer = new TextEditorBuffer();
+
+        buffer.append(null);
+        assertThat(buffer.getText()).isEqualTo("");
+
+        buffer.append("");
+        assertThat(buffer.getText()).isEqualTo("");
+
+        buffer.append("Test");
+        buffer.append("");
+        buffer.append(null);
+        assertThat(buffer.getText()).isEqualTo("Test");
+    }
+
+    @Test
+    void deleteZeroOrNegativeShouldDoNothing() {
+        TextEditorBuffer buffer = new TextEditorBuffer();
+
+        buffer.append("Hello");
+
+        buffer.delete(0);
+        assertThat(buffer.getText()).isEqualTo("Hello");
+
+        buffer.delete(-5);
+        assertThat(buffer.getText()).isEqualTo("Hello");
+    }
+
+    @Test
+    void getLastCharOnEmptyBufferShouldReturnNull() {
+        TextEditorBuffer buffer = new TextEditorBuffer();
+        assertThat(buffer.getLastChar()).isNull();
+
+        buffer.append("A");
+        buffer.delete(1);
+        assertThat(buffer.getText()).isEqualTo("");
+        assertThat(buffer.getLastChar()).isNull();
+    }
 }
 
